9ad07b6ff060a8411a7347b4893c50ce07acca58
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2022-02-13 19:17:38 +0100

    made loader

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,6 @@
 const baseUrl = "https://tribe.api.fdnd.nl/v1";
-
+const loader = document.querySelector(".loader");
+const main = document.querySelector("main");
 
 
 //Functions
@@ -17,63 +18,20 @@ async function getMembers() {
             document.querySelector("#githubHandle").innerHTML = item.githubHandle
             document.querySelector("#bio").innerHTML = item.bio
             document.querySelector("#url").innerHTML =item.url
-            // console.log(item.surname)
+         
+        
             // console.log(item)
         }
         // console.log(item.memberId)
     });
    })
+
+
 //    const data = json.data
 
 //    data.foreach((item) => {
     //    console.log(item)
 //    })
 //    console.log(json.data);
-}
-function fetchtMembers() {
-	data.forEach(element => {
-		if(element.memberId == memberId) {
-			const memberId = element;
-			nameEl.innerHTML = `Hallo, ik ben ${memberId.name}`
-			bioEl.innerHTML = `Hallo, ik ben ${memberId.bio}`
-		}
-	});
-}
-
-function postAndRenderData(){
-    const baseUrl = `${baseUrl/member}`
-    const options = {
-        method:'PATCH',
-        headers: {
-                    'Content-Type': 'application/json',
- 
-        },
-        body: JSON.stringify({
-                     memberId:15,
-                     squadId: 1,
-                     type: "student",
-                     nickname:"SCNnnhMC",
-                     name:"Shauri",
-                     prefix:"",
-                     surname:"Maigua caceres",
-                     avatar:"",
-                     githubHandle:"",
-                     bio:"TESTTT",
-                     url:"",
-
-
-        })
-
-        
-
-    }
-
-    fetch(url,options).then(response => response.json())
-	.then(data => {
-			console.log(data)
-	}).catch(err => {
-			console.log(err)
-	})
-
 }
 

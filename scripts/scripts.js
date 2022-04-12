7c4ac4296df19990a800ce6975cdf2811e629d72
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2022-04-12 22:56:20 +0200

    changed Js function to show data

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,46 +1,39 @@
-const baseUrl = "https://tribe.api.fdnd.nl/v1"
-const loader = document.querySelector(".lds-grid")
-
+const baseUrl = "https://tribe.api.fdnd.nl/v1";
+const loader = document.querySelector(".lds-grid");
+const projectNickname = document.querySelector("#nickName");
+const projectSurname = document.querySelector("#surName");
+const projectGithub = document.querySelector("#githubHandle");
+const projectBio = document.querySelector("#bio");
+const projectUrl = document.querySelector("#url");
 
 //Functions
 
-getMembers()
+getMembers();
 async function getMembers() {
-   const req = await fetch(`${baseUrl}/member`)
-   await req.json()
-   .then(function(json){
-       console.log(json);
-       hidePreloader()
-        json.data.forEach(item => {
-            if(item.memberId == 15) {
-                document.querySelector("#surName").innerHTML = item.surname
-                document.querySelector("#nickName").innerHTML = item.nickname
-                document.querySelector("#githubHandle").innerHTML = item.githubHandle
-                document.querySelector("#bio").innerHTML = item.bio
-                document.querySelector("#url").innerHTML =item.url
-                document.querySelector("avatar").image.src= avatar
-                
-                
-            }
+	const req = await fetch(`${baseUrl}/member`);
+	await req.json().then(function (json) {
+		console.log(json);
+		hidePreloader();
+		json.data.forEach((item) => {
+			if (item.memberId == 15) {
+				// document.querySelector("#surName").innerHTML = item.surname
+				// document.querySelector("#nickName").innerHTML = item.nickname
+				// document.querySelector("#githubHandle").innerHTML = item.githubHandle
+				// document.querySelector("#bio").innerHTML = item.bio
+				// document.querySelector("#url").innerHTML =item.url
+				// document.querySelector("avatar").image.src= avatar
+				projectNickname.innerText = `${item.nickname}`;
+				projectSurname.innerText = `${item.surname}`;
+				projectGithub.innerText = `${item.githubHandle}`;
+				projectBio.innerText = `${item.bio}`;
+				projectUrl.innerText = `${item.url}`;
+			}
+		});
+	});
 
-        });
-    })
-function hidePreloader() {
-    setTimeout(()=>{
-     loader.style.display = "none"
-    }, 1500)
-}
-   
-
-  
-
-
-//    const data = json.data
-
-//    data.foreach((item) => {
-    //    console.log(item)
-//    })
-//    console.log(json.data);
-
-
+	function hidePreloader() {
+		setTimeout(() => {
+			loader.style.display = "none";
+		}, 1500);
+	}
 }

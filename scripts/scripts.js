bdaf7240414cc27cf5b6525ff7d08b774b556381
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2022-04-14 15:34:28 +0200

    added error state and empty state

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,28 +5,55 @@ const projectSurname = document.querySelector("#surName");
 const projectGithub = document.querySelector("#githubHandle");
 const projectBio = document.querySelector("#bio");
 const projectUrl = document.querySelector("#url");
-
+const errorHandling = document.querySelector(".error");
 //Functions
 
-getMembers();
+// getMembers();
+// async function getMembers() {
+// 	const response = await fetch(`${baseUrl}/member`);
+// 	const json = await response.json()
+// 	const member = json.data.find(member => member.memberId == 15)
+// 	hidePreloader();
+// 	renderData(member);	
+// }
+
+getMembers()
 async function getMembers() {
-	const response = await fetch(`${baseUrl}/member`);
-	const json = await response.json()
-	const member = json.data.find(member => member.memberId == 15)
-	hidePreloader();
-	renderData(member);
+    try {
+        const response = await fetch(`${baseUrl}/member`)
+        const json = await response.json()
+        const member = json.data.find(member => member.memberId == 15)
+        hidePreloader();
+		renderData(member)
+    }
+    catch (err) {
+        error();
+        hidePreloader();
+        throw new Error(err);
+    }
 }
 
-function renderData(item) {
-	projectNickname.innerText = `${item.nickname}`;
-	projectSurname.innerText = `${item.surname}`;
-	projectGithub.innerText = `${item.githubHandle}`;
-	projectBio.innerText = `${item.bio}`;
-	projectUrl.innerText = `${item.url}`;
+function renderData(data) {
+	projectNickname.innerText = `${data.nickname}`;
+	projectSurname.innerText = `${data.surname}`;
+	projectGithub.innerText = `${data.githubHandle}`;
+	projectBio.innerText = `${data.bio}`;
+	projectUrl.innerText = `${data.url}`;
 }
 
 function hidePreloader() {
 	setTimeout(() => {
 		loader.style.display = "none";
 	}, 1500);
+}
+
+function error() {
+    setTimeout(() => {
+        errorHandling.style.opacity = 1;
+        errorHandling.style.transform = "translateX(0)";
+        setTimeout(() => {
+            errorHandling.style.opacity = 0
+            errorHandling.style.transform = "translateX(4em)";
+        }, 5000);
+    }, 2100)
 }
